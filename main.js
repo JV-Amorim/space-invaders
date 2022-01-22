4acba75bb90d0ade8d7db02895831fefb8d6c96c
JV-Amorim <joaovitoramorimx@gmail.com>
2022-01-22 18:47:49 -0300

    feat: player rendering

--- main.js
+++ main.js
@@ -1,5 +1,63 @@
-const canvas = document.getElementById('canvas');
-const canvasContext = canvas.getContext('2d');
+const canvasElement = document.getElementById('canvas');
+const canvas = canvasElement.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvasElement.width = window.innerWidth;
+canvasElement.height = window.innerHeight;
+
+class Player {
+  constructor() {
+    const image = this.loadPlayerImage();
+    this.setInitialPropertiesAfterImageLoaded(image);
+  }
+
+  loadPlayerImage() {
+    const image = new Image();
+    image.src = './assets/spaceship.png';
+    return image;
+  }
+
+  setInitialPropertiesAfterImageLoaded(image) {
+    image.onload = () => {
+      this.setPlayerInitialSize(image);
+      this.setPlayerInitialPosition();
+      this.velocity = { x: 0, y: 0 };
+      this.image = image;
+    };
+  }
+
+  setPlayerInitialSize(image) {
+    const scale = 0.15;
+    this.width = image.width * scale;
+    this.height = image.height * scale;
+  }
+
+  setPlayerInitialPosition() {
+    this.position = {
+      x: canvasElement.width / 2 - this.width / 2,
+      y: canvasElement.height - this.height - 20
+    };
+  }
+
+  draw() {
+    if (this.image) {
+      canvas.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  canvas.fillStyle = 'black';
+  canvas.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  player.draw();
+}
+
+const player = new Player();
+
+animate();
