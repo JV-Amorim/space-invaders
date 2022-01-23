ffd93a54f6d9fb79daf9df02c455265bb19a4274
JV-Amorim <joaovitoramorimx@gmail.com>
2022-01-23 14:57:59 -0300

    feat: new InvadersSquadron spawn after a time

--- src/invaders-squadrons-spawner.js
+++ src/invaders-squadrons-spawner.js
@@ -2,19 +2,34 @@ import { InvadersSquadron } from './invaders-squadron.js';
 
 export class InvadersSquadronsSpawner {
   frameCounter = 0;
+  minInstantiationInterval = 500;
+  instantiationInterval = undefined;
   invadersSquadrons = [];
 
   constructor() {
     this.instantiateNewInvadersSquadron();
-  }
-  
-  handleNewFrame() {
-    this.frameCounter++;
-    // TODO - Method implementation.
+    this.generateRandomInstantiationInterval();
   }
 
   instantiateNewInvadersSquadron() {
     this.invadersSquadrons.push(new InvadersSquadron());
+  }
+
+  generateRandomInstantiationInterval() {
+    this.instantiationInterval = Math.floor(Math.random() * 1000);
+    if (this.instantiationInterval < this.minInstantiationInterval) {
+      this.instantiationInterval = this.minInstantiationInterval;
+    }
+  }
+  
+  handleNewFrame() {
+    this.frameCounter++;
+
+    if (this.frameCounter === this.instantiationInterval) {
+      this.instantiateNewInvadersSquadron();
+      this.frameCounter = 0;
+      this.generateRandomInstantiationInterval();
+    }
   }
 
   updateInvadersSquadrons() {
